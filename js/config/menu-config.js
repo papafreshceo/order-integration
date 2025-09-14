72aa60caefa8bf4d51f13ff83a58e59722eeb023
papafreshceo <papafresh.ceo@gmail.com>
2025-09-14 20:03:09 +0900

    Update menu-config.js

--- js/config/menu-config.js
+++ js/config/menu-config.js
@@ -5,7 +5,7 @@ window.MENU_CONFIG = {
         { id: 'realtime', name: '실시간 주문현황' },  // 첫 번째로 이동
         { id: 'dashboard', name: '대시보드' },
         { id: 'search', name: '주문조회' },
-        { id: 'merge', name: '주문통합' },
+        { id: 'merge', name: '주문통합(Excel)' },
         { id: 'invoice', name: '송장등록' },
         { id: 'analytics', name: '통계 분석' }
     ],
@@ -15,3 +15,4 @@ window.MENU_CONFIG = {
         { id: 'search', name: '주문조회' }
     ]
 };
+
